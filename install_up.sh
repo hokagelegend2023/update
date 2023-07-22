1be5e4c2d7e5cc1c069aa4ce48eb34a523210b26
hokagelegend2023 <136045532+hokagelegend2023@users.noreply.github.com>
2023-07-22 18:08:16 +0700

    Update install_up.sh

--- install_up.sh
+++ install_up.sh
@@ -1,1 +1,4 @@
-tes
+#INSTALL UPDATE 
+echo -e " [INFO] Downloading Update File"
+sleep 2
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/hokagelegend2023/beta/main/update/menu.sh" && chmod +x /usr/bin/menu
