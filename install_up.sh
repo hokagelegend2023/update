3e7cd4c9077b02d58dbb174ee7b0e7bbf756d56f
hokagelegend2023 <136045532+hokagelegend2023@users.noreply.github.com>
2023-07-23 00:22:30 +0700

    Update install_up.sh

--- install_up.sh
+++ install_up.sh
@@ -23,6 +23,8 @@ wget -q -O /usr/bin/menu-dns "https://raw.githubusercontent.com/hokagelegend2023
 wget -q -O /usr/bin/info "https://raw.githubusercontent.com/hokagelegend2023/beta/main/update/info.sh" && chmod +x /usr/bin/info
 wget -q -O /usr/bin/mspeed "https://raw.githubusercontent.com/hokagelegend2023/beta/main/update/menu-speedtest.sh" && chmod +x /usr/bin/mspeed
 wget -q -O /usr/bin/mbandwith "https://raw.githubusercontent.com/hokagelegend2023/beta/main/update/menu-bandwith.sh" && chmod +x /usr/bin/mbandwith
+wget -O install-ss-plugin "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/shadowsocks-plugin/install-ss-plugin.sh"
+wget -O ssmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/ssmenu.sh"
 echo -e " [INFO] Update Successfully"
 sleep 2
 exit
